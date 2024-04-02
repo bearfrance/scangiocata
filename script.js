7b565c05f60cbfc9b40a7b08d38fa00314856ed4
bearfrance <darealfrance@gmail.com>
2024-04-02 21:43:04 +0200

    update

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
             target: video
         },
         decoder: {
-            readers: ["code_128_reader", "ean_reader", "upc_reader", "code_39_reader", "code_39_vin_reader", "codabar_reader", "i2of5_reader", "2of5_reader", "code_93_reader"] // Lettori di codici a barre supportati
+            readers: ["code_128_reader"]
         }
     }, function(err) {
         if (err) {
@@ -33,6 +33,13 @@ document.addEventListener('DOMContentLoaded', function() {
     // Quando viene trovato un codice a barre, mostra il risultato
     Quagga.onDetected(function(result) {
         const code = result.codeResult.code;
-        resultDiv.textContent = "Codice a barre trovato: " + code;
+        const type = result.codeResult.format;
+
+        // Controlla se il tipo di codice a barre è Code 128
+        if (type === "code_128") {
+            resultDiv.textContent = "Codice a barre Code 128 trovato: " + code;
+        } else {
+            resultDiv.textContent = "Tipo di codice a barre non supportato: " + type;
+        }
     });
 });
